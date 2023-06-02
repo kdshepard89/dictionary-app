5fb339c1e4504f1f364889ebc6d90584510e4737
Katie S <kdshepard89@gmail.com>
2023-06-02 11:56:13 -0400

    built initial form structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      Hello
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center App-footer">
+          Coded by Katie Shepard and open sourced on{" "}
+          <a href="https://github.com/kdshepard89/dictionary-app">GitHub</a>
+        </footer>
+      </div>
     </div>
   );
 }
